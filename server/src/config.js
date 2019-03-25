87d7a2f673e022e465c1055f6233af7f1dffc0e3
erik.nalbandyan <erik.webdeveloper@gmail.com>
2019-03-25 15:39:06 +0400

    Admin panel commit

--- server/src/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'viewPath': '../src/admin/view/',
-    'frontPath': '/../../front/dist/front'
-};
--- server/src/server.js
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-import app from './app';
-
-const port = process.env.PORT || 8888;
-
-const server = http.createServer(app);
-
-server.listen(port);
