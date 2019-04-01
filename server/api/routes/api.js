a80c0a428cd4e41d94ee3b685ef67fba22dde4e9
Artur <hayrapet2013@gmail.com>
2019-04-01 13:33:03 +0400

    Merge branch 'Admin_panel_integration' of https://github.com/webiAremenia/nika
    # Conflicts:
    #	admin/src/app/content/media/change-media/change-media.component.ts
    #	server/admin/middleware/multer.js

--- server/api/routes/api.js
+++ server/api/routes/api.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const posts = require('./post'),
     sliders = require('./slider'),
     medias = require('./media');
+    contact = require('./contact');
 
 router.use('/post', posts);
 router.use('/slider', sliders);
 router.use('/media', medias);
+router.use('/contact', contact);
 
 module.exports = router;
