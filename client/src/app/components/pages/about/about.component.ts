57e753bf5b988e601ef9d24e21bb5bbe6080d40f
Artur <hayrapet2013@gmail.com>
2019-09-20 17:03:09 +0400

    about animation

--- client/src/app/components/pages/about/about.component.ts
+++ client/src/app/components/pages/about/about.component.ts
@@ -33,10 +33,9 @@ export class AboutComponent implements OnInit, OnDestroy {
 
     @HostListener('wheel', ['$event']) wheel(e) {
         if (window.innerWidth > 992) {
-            if (e.deltaY > 0) {
-                this.initDesktopAnimation(100);
-            } else {
-                this.initDesktopAnimation(-100);
+            const delta = (e.deltaY > 10 || e.deltaY < -10) ? e.deltaY * .7 : null;
+            if (delta) {
+                this.initDesktopAnimation(e.deltaY);
             }
         }
     }
@@ -61,9 +60,9 @@ export class AboutComponent implements OnInit, OnDestroy {
             this.initDesktopAnimation(650);
         }
         if (event.keyCode === 40) {
-            this.initDesktopAnimation(34);
+            this.initDesktopAnimation(70);
         } else if (event.keyCode === 38) {
-            this.initDesktopAnimation(-34);
+            this.initDesktopAnimation(-70);
         }
     }
 
@@ -91,37 +90,30 @@ export class AboutComponent implements OnInit, OnDestroy {
 
     initDesktopAnimation(position: number) {
         this.scrollPosition += position;
-        this.mouseCheck += 1;
-        if (this.mouseCheck === 1) {
-            const clientHeight = document.getElementsByClassName('about-page')[0].clientHeight;
-            if (this.sectionArr.length === 0 && window.innerWidth > 992) {
-                this.initSectionsArr();
-                this.scrollHeight = document.getElementsByClassName('about-scroll')[0].scrollHeight;
-                this.bannerHeight = document.getElementsByClassName('about-page')[0].clientHeight;
-            }
-            if (this.scrollPosition < 0) {
-                this.scrollPosition = 0;
-            } else if (this.scrollPosition > this.scrollHeight) {
-                this.scrollPosition = this.scrollHeight;
-            }
-            for (let i = 0; i < this.sectionArr.length; i++) {
-                if (this.scrollPosition + this.bannerHeight > this.sectionArr[i]) {
-                    document.getElementById('section-' + (i + 1)).style.opacity = '1';
-                }
-            }
-            if (this.scrollPosition + clientHeight >= this.scrollHeight) {
-                for (let i = 0; i < this.sectionArr.length; i++) {
-                    document.getElementById('section-' + (i + 1)).style.opacity = '1';
-                }
-                return this.aboutScroll.nativeElement.style.transform = `translate3d(0, -${this.scrollHeight - clientHeight}px, 0)`;
-            } else if (this.scrollPosition + clientHeight < this.scrollHeight) {
-                this.aboutScroll.nativeElement.style.transform = `translate3d(0, -${this.scrollPosition}px, 0)`;
+        const clientHeight = document.getElementsByClassName('about-page')[0].clientHeight;
+        if (this.sectionArr.length === 0 && window.innerWidth > 992) {
+            this.initSectionsArr();
+            this.scrollHeight = document.getElementsByClassName('about-scroll')[0].scrollHeight;
+            this.bannerHeight = document.getElementsByClassName('about-page')[0].clientHeight;
+        }
+        if (this.scrollPosition < 0) {
+            this.scrollPosition = 0;
+        } else if (this.scrollPosition > this.scrollHeight) {
+            this.scrollPosition = this.scrollHeight;
+        }
+        for (let i = 0; i < this.sectionArr.length; i++) {
+            if (this.scrollPosition + this.bannerHeight > this.sectionArr[i]) {
+                document.getElementById('section-' + (i + 1)).style.opacity = '1';
             }
         }
-        setTimeout(() => {
-            this.mouseCheck = 0;
-        }, 25);
-
+        if (this.scrollPosition + clientHeight >= this.scrollHeight) {
+            for (let i = 0; i < this.sectionArr.length; i++) {
+                document.getElementById('section-' + (i + 1)).style.opacity = '1';
+            }
+            return this.aboutScroll.nativeElement.style.transform = `translate3d(0, -${this.scrollHeight - clientHeight}px, 0)`;
+        } else if (this.scrollPosition + clientHeight < this.scrollHeight) {
+            this.aboutScroll.nativeElement.style.transform = `translate3d(0, -${this.scrollPosition}px, 0)`;
+        }
     }
 
     initMobileAnimation() {
