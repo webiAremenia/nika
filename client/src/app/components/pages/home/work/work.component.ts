7f5b34b487e65fb3ffd22d3ffb296cbd8cc4b886
Artur <hayrapet2013@gmail.com>
2019-09-20 15:14:53 +0400

    all in one

--- client/src/app/components/pages/home/work/work.component.ts
+++ client/src/app/components/pages/home/work/work.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {WorkService} from '../../../../_services/work.service';
 import {Work} from '../../../../_models/work/work';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {Subscription} from 'rxjs';
 import {ResponsiveData} from '../../../../_models/ResponsiveData';
 import {ActionsService} from '../../../../_services/actions.service';
@@ -19,6 +19,7 @@ export class WorkComponent implements OnInit, OnDestroy {
     work: Work;
     slug;
     windowSubscription: Subscription;
+    positionSubscription: Subscription;
     windowSize: ResponsiveData;
     sectionArr: Array<number> = [];
     done = false;
@@ -27,14 +28,14 @@ export class WorkComponent implements OnInit, OnDestroy {
         private actionsService: ActionsService,
         private workService: WorkService,
         private activatedRoute: ActivatedRoute,
+        private router: Router,
         config: AppGlobals
     ) {
         this.imageUrl = config.imageUrl + '/work/';
         this.windowSubscription = actionsService.getWindowSize()
             .subscribe((size: ResponsiveData) => this.windowSize = size);
         this.actionsService.workOpened.next(true);
-        this.actionsService.getWorkScrollPosition().subscribe(pos => {
-            // console.log(pos);
+        this.positionSubscription = this.actionsService.getWorkScrollPosition().subscribe(pos => {
             this.initAnimation(pos);
         });
     }
@@ -54,41 +55,66 @@ export class WorkComponent implements OnInit, OnDestroy {
             this.work = this.workService.works.filter(w => w.slug === this.slug)[0];
             this.workService.current = this.work;
         }
-        // console.log(this.work);
-        this.workService.getOne(this.work.slug).subscribe(d => {
-            this.work.details = d.details;
-            this.done = true;
-        });
+        if (this.work) {
+            this.workService.getOne(this.work.slug).subscribe(d => {
+                this.work.details = d.details;
+                this.done = true;
+            });
+        } else {
+            this.router.navigate(['/']).then();
+        }
     }
 
     initAnimation(position) {
         if (this.sectionArr.length === 0) {
             this.initSectionsArr();
         }
+
+        if (window.innerWidth > 767) {
+            this.animateDesktop(position);
+        } else {
+            this.animateMobile(position);
+        }
+    }
+
+    animateDesktop(position) {
+        console.log(position);
         const bannerHeight = window.innerHeight + 100;
-
         let sum = 0;
-
-        // console.log(55555);
         for (let i = 0; i < this.sectionArr.length; i++) {
             sum += this.sectionArr[i];
-            // console.log(position , -(bannerHeight + sum));
             if (position < -(bannerHeight + sum - 100)) {
                 document.getElementById('section_' + (i + 1)).style.opacity = '1';
             }
         }
+    }
 
+    animateMobile(position) {
+        console.log(this.sectionArr);
+        console.log(position);
+        const bannerHeight = window.innerHeight;
+        let sum = 0;
+        for (let i = 0; i < this.sectionArr.length; i++) {
+            sum += this.sectionArr[i];
+            console.log(position, (bannerHeight + sum));
+            if (position > sum - bannerHeight * .4) {
+                document.getElementById('mob_section_' + (i + 1)).style.opacity = '1';
+            }
+        }
     }
 
     initSectionsArr() {
-        const sectionCount = document.querySelectorAll('.work-dynamic-content-details').length;
-        // const mobSectionCount = document.querySelectorAll('.mobile-components').length;
-        for (let i = 0; i < sectionCount; i++) {
-            this.sectionArr.push(document.getElementById('section_' + (i + 1)).offsetHeight);
+        if (window.innerWidth > 767) {
+            const sectionCount = document.querySelectorAll('.work-dynamic-content-details').length;
+            for (let i = 0; i < sectionCount; i++) {
+                this.sectionArr.push(document.getElementById('section_' + (i + 1)).offsetHeight);
+            }
+        } else {
+            const sectionCount = document.querySelectorAll('.mobile-components').length;
+            for (let i = 0; i < sectionCount; i++) {
+                this.sectionArr.push(document.getElementById('mob_section_' + (i + 1)).offsetHeight);
+            }
         }
-        // for (let i = 0; i < mobSectionCount; i++) {
-        //     this.sectionArr.push(document.getElementById('mob_section_' + (i + 1)).offsetHeight);
-        // }
     }
 
     ngOnDestroy(): void {
@@ -96,5 +122,7 @@ export class WorkComponent implements OnInit, OnDestroy {
         this.sectionArr = [];
         this.actionsService.workOpened.next(false);
         this.actionsService.workScrollPosition.next(-window.innerHeight + 100);
+        this.windowSubscription.unsubscribe();
+        this.positionSubscription.unsubscribe();
     }
 }
